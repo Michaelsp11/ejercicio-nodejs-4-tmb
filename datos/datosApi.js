4eae30995b2f14c539b820e2c5ae0182a4a25cae
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-04 18:37:09 +0200

    Estaciones se muestran + coordenadas + fecha inauguración + abrev + refactorizado pintarEstaciones

--- datos/datosApi.js
+++ datos/datosApi.js
@@ -7,7 +7,14 @@ const coordenadasMetros = async (linia) => {
   const data = await response.json();
   return data;
 };
-
+const paradasMetro = async (linia) => {
+  const response = await fetch(
+    `${process.env.URL_METRO}${linia}/estacions?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`
+  );
+  const data = await response.json();
+  return data;
+};
 module.exports = {
   coordenadasMetros,
+  paradasMetro,
 };
